f694b41444de7df698fcbf126a9dc62beee8edd9
DARYA <suleimanava>
2019-03-01 00:39:26 +0300

    lesson 2 task b3

--- src/by/it/suleimanava/lesson02/TaskB3.java
+++ src/by/it/suleimanava/lesson02/TaskB3.java
@@ -10,16 +10,19 @@ package by.it.suleimanava.lesson02;
 */        
 class TaskB3 {
     public static void main(String[] args) {
-//        String s = "C";
-//        System.out.println("Новым Годом");
-//        System.out.println("C Новым");
-//        System.out.println("Годом");
-//        System.out.print("C Новым");
-//        System.out.println("Годом");
-//        System.out.println(s);
-//        System.out.print("C Новым!");
-//        System.out.print(" ");
-//        System.out.println("Годом");
+
+       String s = "C";
+        System.out.print(s);
+        System.out.print(" ");
+        System.out.print("Новым");
+        System.out.print(" ");
+        System.out.print("Годом");
+        System.out.print(" ");
+        System.out.print(" ");
+        System.out.print(" ");
+       System.out.print(" ");
+
+
     }
 }
 
