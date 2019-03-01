81f4e78b67cb72fd59e340efa4e24c5c9c39e8d0
hramcov <Tarantul-69>
2019-03-01 19:37:10 +0300

    lesson4 task A +b1

--- src/by/it/chramtsov/lesson04/TaskB2.java
+++ src/by/it/chramtsov/lesson04/TaskB2.java
@@ -1,5 +1,7 @@
 package by.it.chramtsov.lesson04;
 
+import java.util.Scanner;
+
 /*
 Таблица умножения
 Вывести на экран таблицу умножения size х size используя цикл while.
@@ -24,5 +26,17 @@ size прочитать c клавиатуры. Числа разделить п
 3. Программа должна выводить таблицу умножения size х size.
  */
 public class TaskB2 {
+    public static void main(String[] args) {
+          Scanner sc;
+        sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int x;
+        while (int x<=i);
+        for (int x = 1; x <=i ; x++) {
+            System.out.println();
+
+
+        }
+    }
 
 }
