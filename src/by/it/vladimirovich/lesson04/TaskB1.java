74a2771cd1b3b5d73a230bd3ddac8e509637b96d
SlavaVladimirovich <47827379+SlavaVladimirovich@users.noreply.github.com>
2019-03-05 01:53:27 +0300

    less4

--- src/by/it/vladimirovich/lesson04/TaskB1.java
+++ src/by/it/vladimirovich/lesson04/TaskB1.java
@@ -1,5 +1,7 @@
 package by.it.vladimirovich.lesson04;
 
+import java.util.Scanner;
+
 /*
 Определите количество дней в году
 
@@ -27,6 +29,12 @@ package by.it.vladimirovich.lesson04;
 */
 public class TaskB1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        if (year % 100 == 0 & year % 400 != 0) System.out.println("количество дней в году: 365");
+        else if (year % 4 == 0) System.out.println("количество дней в году: 366");
+        else System.out.println("количество дней в году: 365");
+
 
     }
 
--- src/by/it/vladimirovich/lesson04/TaskB3.java
+++ src/by/it/vladimirovich/lesson04/TaskB3.java
@@ -25,7 +25,26 @@ package by.it.vladimirovich.lesson04;
  */
 
 
+import java.util.Scanner;
 
 public class TaskB3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int sum=1;
+        int b=2;
+        for (int i = 1; i <=a ; i++) {
+            System.out.print(sum+",");
+            sum=sum+b;
+            b++;
+
+
+        }
+
+
+            
+
+
+    }
 
 }
