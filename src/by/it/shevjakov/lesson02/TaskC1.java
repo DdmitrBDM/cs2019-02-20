f19fae0ca464229ac5d99ab27b82cb44e45d5902
IvanShevy <ivan.shevy@gmail.com>
2019-03-16 12:01:52 +0300

    lesson02-TaskC1-Done

--- src/by/it/shevjakov/lesson02/TaskC1.java
+++ src/by/it/shevjakov/lesson02/TaskC1.java
@@ -10,7 +10,16 @@ package by.it.shevjakov.lesson02;
 Sum = 60
 
 */
+import java.util.Scanner;
+
 class TaskC1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a+b;
+        System.out.println("Sum"+" "+"="+" "+c);
+    }
 
 
 
