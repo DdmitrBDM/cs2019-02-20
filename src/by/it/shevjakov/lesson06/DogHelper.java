3b9efe7003430af8746e3ea975cd414203833261
IvanShevy <ivan.shevy@gmail.com>
2019-03-17 18:54:24 +0300

    lesson06-in_process

--- src/by/it/shevjakov/lesson06/DogHelper.java
+++ src/by/it/shevjakov/lesson06/DogHelper.java
@@ -2,19 +2,17 @@ package by.it.shevjakov.lesson06;
 
 class DogHelper {
 
-    static void printAllNames(Dog[] dogs) {
+    static void printAllNames(Dog[] dogs){
         for (Dog dog : dogs) {
             System.out.print(dog.getName()+" ");
         }
-        System.out.println();
     }
-
-    static double averageAge(Dog[] dogs) {
-        double sum = 0;
+    static double averageAge(Dog[] dogs){
+        double sum=0;
         for (Dog dog : dogs) {
-            sum = sum + dog.getAge();
+            sum=sum+dog.getAge();
         }
-        return sum / dogs.length;
+        return sum/dogs.length;
     }
 }
 
