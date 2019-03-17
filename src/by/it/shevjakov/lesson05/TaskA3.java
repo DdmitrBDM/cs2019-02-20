bededd1de612899230ae8a7d6d039e85385e19be
IvanShevy <ivan.shevy@gmail.com>
2019-03-17 11:43:12 +0300

    lesson05-Task-A3-Done

--- src/by/it/shevjakov/lesson05/TaskA3.java
+++ src/by/it/shevjakov/lesson05/TaskA3.java
@@ -1,14 +1,11 @@
 package by.it.shevjakov.lesson05;
-
 /* Массив из чисел в обратном порядке
 1. Создать массив на 10 чисел.
 2. Ввести с клавиатуры 10 целых чисел и записать их в массив.
 3. Найти минимальный и максимальный элементы массива.
 4. Вывести эти два элемента на экран, через пробел.
-
 Например, для такого ввода
 1 2 3 4 5 6 7 8 9 0
-
 вывод ожидается такой
 0 9
 */
@@ -17,21 +14,20 @@ import java.util.Scanner;
 
 public class TaskA3 {
 
-        public static void main(String[] args) {
+    public static void main(String[] args) {
         int[] arr = new int[10];
         Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
         }
-        int min=arr[0];
-        int max=arr[0];
-
+        int min = arr[0];
+        int max = arr[0];
         for (int element : arr) {
-            if (element<min)
-                min=element;
-            if (element>max)
-                max=element;
+            if (element < min)
+                min = element;
+            if (element > max)
+                max = element;
         }
-            System.out.println(min+" "+max);
-        }
+        System.out.println(min + " " + max);
+    }
 }
