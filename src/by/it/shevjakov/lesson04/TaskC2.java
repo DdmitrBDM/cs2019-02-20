f50bd57832685df6b2b49dfda1b2bdba238d76fd
IvanShevy <ivan.shevy@gmail.com>
2019-03-17 11:27:14 +0300

    lesson04-Task-C2-Done

--- src/by/it/shevjakov/lesson04/TaskC2.java
+++ src/by/it/shevjakov/lesson04/TaskC2.java
@@ -1,31 +1,37 @@
 package by.it.shevjakov.lesson04;
-
 /*
-
 На входе вводится число и месяц 2018 года.
 Программа должна выводить номер дня года, считая 1 января первым днем,
 а 31 декабря 365-м.
-
 Пример:
 Ввод:
 1 1
-
 Вывод
 1
-
 Ввод:
 31 12
-
 Вывод
 365
-
 Ввод:
 13 9
-
 Вывод
 256
-
  */
-public class TaskC2 {
 
+import java.util.Scanner;
+
+public class TaskC2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a;
+        for (byte c = 2; c <= b; c++) {
+            if (c == 2 || c == 4 || c == 6 || c == 8 || c == 9 || c == 11) sum = sum + 31;
+            if (c == 3) sum = sum + 28;
+            if (c == 5 || c == 7 || c == 10 || c == 12) sum = sum + 30;
+        }
+        System.out.println(sum);
+    }
 }
+
